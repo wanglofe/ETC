83a6b66d7a897845ee4c160606d3eaadfbef3b89
wanglofe <1252189089@qq.com>
2017-10-02 06:07:01 -0400

    Update debug.h

--- C/debug/debug.h
+++ C/debug/debug.h
@@ -6,11 +6,13 @@
 #ifdef _DEBUG
 #define debug(format, arg...) printf(format, ##arg)
 #define debug_verbose(format, arg...) printf("[%s, %s: %d]: "format, __FILE__, __func__, __LINE__,  ##arg)
+#define print(format, arg...) printf(format"\n", ##arg)
 #else
 #define debug(format, arg...)
 #define debug_verbose(format, arg...)
+#define print(format, arg...)
 #endif
 
-#endif
+#endif //debug.h
 
 
